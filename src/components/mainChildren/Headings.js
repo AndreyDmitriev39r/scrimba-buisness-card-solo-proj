4e588bd5641778cae4501bcdc3208ca18e4bc381
Andrey Dmitriev <admit8490@gmail.com>
2023-02-22 17:08:32 +0200

    Headings component ready

--- src/components/mainChildren/Headings.js
+++ src/components/mainChildren/Headings.js
@@ -1,6 +1,10 @@
 function Headings() {
   return (
-    <h1>This is Headings</h1>
+    <section className="headings-section">
+    <h1 className="main-heading">Cappy the Coconut</h1>
+    <h3 className="sub-heading">Semi-aquatic Overlord</h3>
+    <h5 className="website">cappythecoconut.website</h5>
+    </section>
   )
 }
 
