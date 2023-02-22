578fc7332a8533f2343ef008983ce1fcc9528858
Andrey Dmitriev <admit8490@gmail.com>
2023-02-22 22:08:03 +0200

    fixes-for-deployment-testing

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,5 @@
-import avatar from '../images/brian-mcgowan-P1-6ioOcGNU-unsplash.jpg'
+import avatar from '../images/hero-image.jpg'
 
-// src\images\brian-mcgowan-P1-6ioOcGNU-unsplash.jpg
 function Image() {
   return (
     <img className='avatar' src={avatar} alt='capybara avatar'/>
